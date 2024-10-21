e5754c1b70cf7178cae6c12cf094125041cfae08
muhmmad-zeeshan <rafaq657@gmail.com>
2024-10-21 22:14:40 +0500

    Initial Commit

---
+++ RequestPermissionLearning/RequestPermissionLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequestPermissionLearningApp.swift
+//  RequestPermissionLearning
+//
+//  Created by Muhammad Zeeshan on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RequestPermissionLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
